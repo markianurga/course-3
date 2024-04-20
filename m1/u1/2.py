601b175c9bb51f9c82dc9640abaa04606162bfa6
Markiian Yurga <markianurga5@gmail.com>
2024-04-20 14:22:18 +0300

    навчилиля користуватися elif, else.

--- m1/u1/2.py
+++ m1/u1/2.py
@@ -1,5 +1,4 @@
 uah  = int(input('Введіть суму гривнях'))
 usd = int(input('Введіть курс долара'))
-
-print(uah '.') 
-print('Компанія, яка була заснована day, year року.')
+ret = uah / usd
+print(ret) 

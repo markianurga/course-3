6cd19b188e597dcc6c21e448a213e8e7e91b59bb
Markiian Yurga <markianurga5@gmail.com>
2024-05-04 14:24:59 +0300

    ми навчилися питати рік нароження і ім'я від рік нароження  віднімати поточний рік

---
+++ m1/u4/0.py
@@ -0,0 +1,11 @@
+def farm(up) :
+    print(up)
+    
+farm("yes")
+farm("neme markian")
+
+def cyma(up, cyo) :
+    print(up + cyo)
+    
+cyma(3, 7)
+cyma(999999999999999999, 99999999999999999999)
---
+++ m1/u4/1.py
@@ -0,0 +1,12 @@
+def pof(aup,op):
+    print("helo " + aup, 2024 - op)
+    return 2024 - op
+    
+    
+    
+    
+    
+iu = pof( " mark", 2011)
+red = pof("vova", 2003)
+
+print(iu + red)

939d9832b77234ee17a2e836b48c7f6a7a282d0c
Markiian Yurga <markianurga5@gmail.com>
2024-06-29 12:23:50 +0300

    створили макати на екранни і шоб вони переклучалися

--- m2/TestRuffier/main.py
+++ m2/TestRuffier/main.py
@@ -12,61 +12,118 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 class my_Screen1(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
-        texte = Label(text = "gsgregfegegdgrgrrrd")
+        neme = TextInput(text = "",multiline=False, size_hint = (0.9,0.9))
+        vik = TextInput(text = "",multiline=False, size_hint = (0.9,0.9))
+        texte = Label(text = txt_instruction)
+        textp = Label(text = "ведіть імя")
+        textpy = Label(text = "ведіть вік")
         red = Button(text = "продовшити")
+        
         lauyte_v = BoxLayout(orientation = "vertical")
+        lauyte_h = BoxLayout()
+        lauyte_h2 = BoxLayout()
+        
         lauyte_v.add_widget(texte)
+        lauyte_h.add_widget(textp)
+        lauyte_h.add_widget(neme)
+        lauyte_h2.add_widget(textpy)
+
+        lauyte_h2.add_widget(vik)
+
+                
+        lauyte_v.add_widget(lauyte_h)
+        lauyte_v.add_widget(lauyte_h2)
         lauyte_v.add_widget(red)
         self.add_widget(lauyte_v)
+        red.on_press = self.next
+        
+    def next(self):
+        self.manager.current = "s2"
 
 class my_Screen2(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
-        text = Label(text = "щощозщ")
-        redу = Button(text = "продовшити")
+        text = Label(text = txt_test1)
+        red = Button(text = "продовшити")
+        texerw = TextInput(text = "")
+        tevkdf = Label(text = "ведіть результат",)
         lauyte_v = BoxLayout(orientation = "vertical")
+        lauyte_h_2 = BoxLayout()
         lauyte_v.add_widget(text)
-        lauyte_v.add_widget(redу)
+        lauyte_v.add_widget(lauyte_h_2)
+        lauyte_h_2.add_widget(tevkdf)
+        lauyte_h_2.add_widget(texerw)
+        lauyte_v.add_widget(red)
         self.add_widget(lauyte_v)
+        red.on_press = self.next
+        
+    def next(self):
+        self.manager.current = "s3"
 
+ 
 class my_Screen3(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
-        texteu = Label(text = "")
-        redys = Button(text = "продовшити")
+        texteu = Label(text = txt_test2)
+        red = Button(text = "продовшити")
         lauyte_v = BoxLayout(orientation = "vertical")
         lauyte_v.add_widget(texteu)
-        lauyte_v.add_widget(redys)
+        lauyte_v.add_widget(red)
         self.add_widget(lauyte_v)
+        red.on_press = self.next
+        
+    def next(self):
+        self.manager.current = "s4"
+
 
 class my_Screen4(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
-        textet = Label(text = "")
-        redo = Button(text = "продовшити")
+        textet = Label(text = txt_test3)
+        red = Button(text = "продовшити")
+        neme = TextInput(text = "",multiline=False, size_hint = (0.9,0.9))
+        vik = TextInput(text = "",multiline=False, size_hint = (0.9,0.9))
+        textp = Label(text = "результат")
+        textpy = Label(text = "результат після відпочинку")
         lauyte_v = BoxLayout(orientation = "vertical")
+        lauyte_h_4 = BoxLayout()
+        lauyte_h_23 = BoxLayout()
         lauyte_v.add_widget(textet)
-        lauyte_v.add_widget(redo)
+        lauyte_h_4.add_widget(textp)
+        lauyte_h_4.add_widget(neme)
+        lauyte_h_23.add_widget(textpy)
+        lauyte_h_23.add_widget(vik)
+        lauyte_v.add_widget(lauyte_h_4)
+        lauyte_v.add_widget(lauyte_h_23)
+        lauyte_v.add_widget(red)
         self.add_widget(lauyte_v)
+        red.on_press = self.next
+        
+    def next(self):
+        self.manager.current = "s5"
+
+
 
 class my_Screen5(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
         texter = Label(text = "")
-        redt = Button(text = "продовшити")
+        texte = Label(text = "")
+        textero = Label(text = "")
         lauyte_v = BoxLayout(orientation = "vertical")
         lauyte_v.add_widget(texter)
-        lauyte_v.add_widget(redt)
+        lauyte_v.add_widget(texte)
+        lauyte_v.add_widget(textero)
         self.add_widget(lauyte_v)
  
 class my_app(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(my_Screen1(name = "s1"))
-        sm.add_widget(my_Screen2(name = "se2"))
-        sm.add_widget(my_Screen3(name = "re3"))
-        sm.add_widget(my_Screen4(name = "so4"))
-        sm.add_widget(my_Screen5(name = "sa5"))
+        sm.add_widget(my_Screen2(name = "s2"))
+        sm.add_widget(my_Screen3(name = "s3"))
+        sm.add_widget(my_Screen4(name = "s4"))
+        sm.add_widget(my_Screen5(name = "s5"))
         
         
         

b02b88ea6bcb07ffb05ecaf5c48c528136287d04
Markiian Yurga <markianurga5@gmail.com>
2024-08-31 12:11:01 +0300

    навчилися вчити робота

--- m4/u1/1.py
+++ m4/u1/1.py
@@ -1,3 +1,7 @@
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 import pandas as pd
 df = pd.read_csv('m4/u1/titanic.csv')
 print(df.info())
@@ -22,3 +26,20 @@ def rkfrmkom(Sex):
         return 0 
 df['Sex']=df ['Sex'].apply(rkfrmkom)
 print(df.info())
+
+df[list(pd.get_dummies(df['Embarked']).columns)] = pd.get_dummies(df['Embarked'])
+df.drop('Embarked', axis = 1,
+        inplace = True)
+print(df.info())
+print(df)
+
+
+x = df.drop('Survived', axis = 1)
+y = df['Survived']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25)
+
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+print(x_test, x_train)

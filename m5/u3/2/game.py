cc7109636e6132cb7b363f918cda1aadb4d47d56
Markiian Yurga <markianurga5@gmail.com>
2024-10-12 18:29:16 +0300

    навчилися зспавнити чанки

--- m5/u3/2/game.py
+++ m5/u3/2/game.py
@@ -1,1 +1,17 @@
 # напиши тут код основного вікна гри
+from direct.showbase.ShowBase import ShowBase
+from mapmanager import Mapmanager
+
+
+class Game(ShowBase):
+    def __init__ (self):
+        super().__init__()
+        self.land = Mapmanager()
+        self.land.loadLand('m5/u3/2/land3.txt')
+        base.camLens.setFov(90)
+        
+
+
+
+ret = Game() 
+ret.run()
--- m5/u3/2/mapmanager.py
+++ m5/u3/2/mapmanager.py
@@ -1,1 +1,26 @@
 # напиши тут код створення та управління карткою
+class Mapmanager:
+    def __init__ (self):
+        self.model = 'block'
+        self.texture = 'stone.png'
+        self.startNew()
+#        self.addblock((1,4,1))    
+#        self.addblock((0,4,-1))
+    def addblock(self, position):
+        self.block = loader.loadModel(self.model)
+        self.block.setTexture( loader.loadTexture(self.texture))
+        self.block.setPos(position)
+        self.block.reparentTo(self.land)
+    def startNew (self):
+        self.land = render.attachNewNode("land")
+    def loadLand(self, filename):
+        with open(filename) as file:
+            y = 0
+            for line in file:
+                x = 0
+                line = line.split(' ')
+                for z in line:
+                    for z0 in range(int(z)+1):
+                        self.addblock((x, y, z0))
+                    x = x + 1
+                y = y + 1

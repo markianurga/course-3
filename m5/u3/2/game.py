ffa52c23cf695543711433516abf9ca04164d448
Markiian Yurga <markianurga5@gmail.com>
2024-10-19 11:31:54 +0300

    навчилися робити постережиння від різних позицій

--- m5/u3/2/game.py
+++ m5/u3/2/game.py
@@ -1,13 +1,15 @@
 # напиши тут код основного вікна гри
 from direct.showbase.ShowBase import ShowBase
 from mapmanager import Mapmanager
+from hero import Hero
 
 
 class Game(ShowBase):
     def __init__ (self):
         super().__init__()
         self.land = Mapmanager()
-        self.land.loadLand('m5/u3/2/land3.txt')
+        self.land.loadLand('m5/u3/2/land.txt')
+        self.hero = Hero((0,0,4),self.land)
         base.camLens.setFov(90)
         
 
--- m5/u3/2/hero.py
+++ m5/u3/2/hero.py
@@ -1,1 +1,43 @@
 # напиши свій код тут
+class Hero:
+    def __init__(self, pos , land):
+        self.land = land
+        self.model =  loader.loadModel('ant')
+        self.model.reparentTo(render)
+        self.model.setPos(pos)
+        self.model.setScale(0.66)
+        
+        self.camerabind()
+        self.accept_events()
+        
+        
+    def camerabind (self):
+        base.disableMouse()
+        base.camera.reparentTo(self.model)
+#       base.camera.setPos()
+        self.comeraOn = True
+    
+    def cameraAp(self):
+        base.enableMouse()
+        base.camera.reparentTo(render)
+        xyz = self.model.getPos()
+        base.mouseInterfaceNode.setPos(xyz)
+        
+        
+        self.comeraOn = False
+
+       
+    def accept_events(self):
+        base.accept('c',self.changeView)
+        
+        
+    def changeView(self):  
+        if self.comeraOn:
+            self.cameraAp()
+        else:
+            self.camerabind()
+        
+        
+        
+        
+        

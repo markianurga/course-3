70498a50a1c56ed7bda81265006d5df843f508ae
Markiian Yurga <markianurga5@gmail.com>
2025-01-31 21:03:13 +0200

    оптимізували базу даних

--- m6/u4/1.py
+++ m6/u4/1.py
@@ -1,20 +1,29 @@
 import sqlite3
 from pprint import pprint
 
-conn = sqlite3.connect("m6/u4/Quiz.db")
-cursor = conn.cursor()
+conn = None
+cursor = None
 
+def open():
+    global conn, cursor
+    conn = sqlite3.connect("m6/u4/Quiz.db")
+    cursor = conn.cursor()
+
+def do (query):
+    cursor.execute(query)
+    conn.commit()
+open()
 query = """
     CREATE TABLE IF NOT EXISTS quiz  (
-        id INTEGER,
+        id INTEGER primary key,
         name VARCHAR)
 """
-cursor.execute(query)
-conn.commit()
+
+do (query)
  
 query = """
     CREATE TABLE IF NOT EXISTS  question  (
-        id INTEGER,
+        id INTEGER primary key,
         Question VARCHAR,
         answer VARCHAR,
         wrong1 VARCHAR,
@@ -22,6 +31,20 @@ query = """
         wrong3 VARCHAR
     )
 """
-cursor.execute(query)
-conn.commit()
+
+do (query)
+ 
+query = """
+    CREATE TABLE IF NOT EXISTS  quiz_content (
+        id INTEGER primary key,
+        quiz_id INTEGER,
+        question_id INTEGER,
+        FOREIGN KEY (quiz_id) REFERENCES quiz (id),
+        FOREIGN KEY (question_id) REFERENCES question (id)
+
+    )
+"""
+
+do (query)
+
  
